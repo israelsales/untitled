c7169e30a347042269a58e711d1f864e5823a2f7
Israel Sales <raell.com@gmail.com>
2017-06-14 13:47:06 -0300

    Initial commit

---
+++ src/com/projeto/app/Teste.java
@@ -0,0 +1,17 @@
+package com.projeto.app;
+
+import java.util.Scanner;
+
+/**
+ * Created by Israel Sales on 14/06/2017.
+ */
+public class Teste {
+
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Entre com um nome: ");
+        String nome = scanner.nextLine();
+        System.out.println(nome);
+
+    }
+}
